e7367ce7ea69e6dc3d5c9b907b4df115ab0d688b
Seif Kungulio <seif.kungulio@gmail.com>
2025-05-03 03:41:44 -0500

    Modified final project file in the final project directory

--- DSCI-512/FinalProject/FinalProject_Kungulio_Seif.R
+++ DSCI-512/FinalProject/FinalProject_Kungulio_Seif.R
@@ -57,80 +57,143 @@ library(neuralnet)
 ################################################################################
 ##  1.  Data Preparation.                                                      #
 ################################################################################
-##    1.a.  Load the dataset insurance.csv into memory.
+##      a.  Load the dataset insurance.csv into memory.
+insurance <- read.csv("data/insurance.csv")
+
+dim(insurance)
+
+head(insurance)
 
 
-##    1.b.  In the data frame, transform the variable charges by setting 
+##      b.  In the data frame, transform the variable charges by setting 
 ##          insurance$charges = log(insurance$charges). Do not transform it 
 ##          outside of the data frame.
+insurance$charges <- log(insurance$charges)
+
+# Display the first ten occurrences
+head(insurance$charges, 10)
 
 
-##    1.c.  Using the data set from 1.b, use the model.matrix() function to 
+##      c.  Using the data set from 1.b, use the model.matrix() function to 
 ##          create another data set that uses dummy variables in place of 
 ##          categorical variables. Verify that the first column only has 
 ##          ones (1) as values, and then discard the column only after 
 ##          verifying it has only ones as values.
+insurance_dummy <- model.matrix(charges ~ ., data = insurance)
+
+# Verify first column has only 1's
+all(insurance_dummy[, 1] == 1)
+
+# Display the first ten rows
+head(insurance_dummy[, 1], 10)
+
+# Remove the intercept column (first column)
+insurance_dummy <- insurance_dummy[, -1]
 
 
-##    1.d.  Use the sample() function with set.seed equal to 1 to generate 
+##      d.  Use the sample() function with set.seed equal to 1 to generate 
 ##          row indexes for your training and tests sets, with 2/3 of the 
 ##          row indexes for your training set and 1/3 for your test set. 
 ##          Do not use any method other than the sample() function for 
 ##          splitting your data.
+set.seed(1)
+
+# Generate row indexes
+sample_index <- sample(1:nrow(insurance), nrow(insurance) * 2/3)
 
 
-##    1.e.  Create a training and test data set from the data set created 
+##      e.  Create a training and test data set from the data set created 
 ##          in 1.b using the training and test row indexes created in 1.d. 
 ##          Unless otherwise stated, only use the training and test data sets 
 ##          created in this step.
+train_data <- insurance[sample_index, ]
+test_data <- insurance[-sample_index, ]
 
 
-##    1.f.  Create a training and test data set from data set created in 
+##      f.  Create a training and test data set from data set created in 
 ##          1.c using the training and test row indexes created in 1.d
-
+train_dummy <- insurance_dummy[sample_index, ]
+test_dummy <- insurance_dummy[-sample_index, ]
 
 
 
 ################################################################################
 ##  2.  Build a multiple linear regression model.                              #
 ################################################################################
-##    2.a.  Perform multiple linear regression with charges as the response and 
+##      a.  Perform multiple linear regression with charges as the response and 
 ##          the predictors are age, sex, bmi, children, smoker, and region. 
 ##          Print out the results using the summary() function. Use the 
 ##          training data set created in step 1.e to train your model.
+set.seed(1)
+lm_model <- lm(charges ~ age + sex + bmi + children + smoker + region, 
+               data = train_data)
+
+summary(lm_model)
 
 
-##    2.b.  Is there a relationship between the predictors and the response?
+##      b.  Is there a relationship between the predictors and the response?
 
 
-##    2.c.  Does sex have a statistically significant relationship to the 
+##      c.  Does sex have a statistically significant relationship to the 
 ##          response?
 
 
-##    2.d.  Perform best subset selection using the stepAIC() function from 
+##      d.  Perform best subset selection using the stepAIC() function from 
 ##          the MASS library, choose best model based on AIC. For the "direction"
 ##          parameter in the stepAIC() method, set direciton="backward"
+lm_best <- stepAIC(lm_model, direction = "backward")
+
+summary(lm_best)
 
 
-##    2.e.  Compute the test error of the best model in #3d based on AIC using 
+##      e.  Compute the test error of the best model in #3d based on AIC using 
 ##          LOOCV using trainControl() and train() from the caret library. 
 ##          Report the MSE by squaring the reported RMSE.
+ctrl_loocv <- trainControl(method = "LOOCV")
+
+set.seed(1)
+lm_loocv <- train(charges ~ age + bmi + children + smoker + region, 
+                  data = train_data, 
+                  method = "lm", 
+                  trControl = ctrl_loocv)
+
+mse_loocv <- lm_loocv$results$RMSE^2
+
+mse_loocv
 
 
-##    2.f.  Calculate the test error of the best model in #3d based on AIC 
+##      f.  Calculate the test error of the best model in #3d based on AIC 
 ##          using 10-fold Cross-Validation. Use train and trainControl from 
 ##          the caret library. Refer to model selected in #3d based on AIC. 
 ##          Report the MSE.
+ctrl_cv10 <- trainControl(method = "cv", number = 10)
+
+set.seed(1)
+lm_cv10 <- train(charges ~ age + bmi + children + smoker + region, 
+                 data = train_data, 
+                 method = "lm", 
+                 trControl = ctrl_cv10)
+
+mse_cv10 <- lm_cv10$results$RMSE^2
+
+mse_cv10
 
 
-##    2.g.  Calculate and report the test MSE using the best model from 2.d 
+##      g.  Calculate and report the test MSE using the best model from 2.d 
 ##          and test data set created in step 1.e.
+pred_lm <- predict(lm_best, newdata = test_data)
+
+mse_test_lm <- mean((test_data$charges - pred_lm)^2)
+
+mse_test_lm
 
 
-##    2.h.  Compare the test MSE calculated in step 2.f using 10-fold 
+##      h.  Compare the test MSE calculated in step 2.f using 10-fold 
 ##          cross-validation with the test MSE calculated in step 2.g. 
 ##          How similar are they?
-
+print(c(LOOCV = mse_loocv, 
+        CV10 = mse_cv10, 
+        Test = mse_test_lm))
 
 
 
@@ -140,10 +203,16 @@ library(neuralnet)
 ##    3.a   Build a regression tree model using function tree(), where charges 
 ##          is the response and the predictors are age, sex, bmi, children, 
 ##          smoker, and region.
+tree_model <- tree(charges ~ age + sex + bmi + children + smoker + region, 
+                   data = train_data)
 
 
 ##    3.b.  Find the optimal tree by using cross-validation and display the 
 ##          results in a graphic. Report the best size.
+cv_tree <- cv.tree(tree_model)
+plot(cv_tree$size, cv_tree$dev, type = "b", xlab = "Tree Size", ylab = "Deviance")
+
+best_size <- 3
 
 
 ##    3.c.  Justify  the number you picked for the optimal tree with regard to 
@@ -151,13 +220,20 @@ library(neuralnet)
 
 
 ##    3.d.  Prune the tree using the optinal size found in 3.b.
+pruned_tree <- prune.tree(tree_model, best = best_size)
 
 
 ##    3.e.  Plot the best tree model and give labels.
+plot(pruned_tree)
+text(pruned_tree, pretty = 0)
 
 
 ##    3.f.  Calculate the test MSE for the best model.
+pred_tree <- predict(pruned_tree, newdata = test_data)
 
+mse_test_tree <- mean((test_data$charges - pred_tree)^2)
+
+mse_test_tree
 
 
 
@@ -167,17 +243,26 @@ library(neuralnet)
 ##    4.a.  Build a random forest model using function randomForest(), where 
 ##          charges is the response and the predictors are age, sex, bmi, 
 ##          children, smoker, and region.
+set.seed(1)
+rf_model <- randomForest(charges ~ age + sex + bmi + children + smoker + region, 
+                         data = train_data, importance = TRUE)
 
 
 ##    4.b.  Compute the test error using the test data set.
+pred_rf <- predict(rf_model, newdata = test_data)
+
+mse_test_rf <- mean((test_data$charges - pred_rf)^2)
+
+mse_test_rf
 
 
 ##    4.c.  Extract variable importance measure using the importance() function.
+importance(rf_model)
 
 
 ##    4.d.  Plot the variable importance using the function, varImpPlot(). 
 ##          Which are the top 3 important predictors in this model?
-
+varImpPlot(rf_model)
 
 
 
@@ -187,23 +272,36 @@ library(neuralnet)
 ##    5.a.  The response is charges and the predictors are age, sex, bmi, 
 ##          children, smoker, and region. Please use the svm() function with 
 ##          radial kernel and gamma=5 and cost = 50.
-
+set.seed(1)
+svm_model <- svm(charges ~ age + sex + bmi + children + smoker + region, 
+                 data = train_data, kernel = "radial", gamma = 5, cost = 50)
 
 ##    5.b.  Perform a grid search to find the best model with potential 
 ##          cost: 1, 10, 50, 100 and potential gamma: 1,3 and 5 and potential 
 ##          kernel: "linear","radial" and "sigmoid". And use the training set 
 ##          created in step 1.e.
+svm_tune <- tune(svm, charges ~ ., data = train_data,
+                 ranges = list(kernel = c("linear", "radial", "sigmoid"),
+                               cost = c(1, 10, 50, 100),
+                               gamma = c(1, 3, 5)))
 
 
 ##    5.c.  Print out the model results. What are the best model parameters?
+summary(svm_tune)
+
+best_svm <- svm_tune$best.model
+
+best_svm
 
 
 ##    5.d.  Forecast charges using the test dataset and the best model 
 ##          found in c).
-
+pred_svm <- predict(best_svm, newdata = test_data)
 
 ##    5.e.  Compute the MSE (Mean Squared Error) on the test data.
+mse_test_svm <- mean((test_data$charges - pred_svm)^2)
 
+mse_test_svm
 
 
 
@@ -212,17 +310,22 @@ library(neuralnet)
 ################################################################################
 ##    6.a.  Remove the sex, smoker, and region, since they are not numerical 
 ##          values.
+insurance_numeric <- insurance[, c("age", "bmi", "children", "charges")]
 
 
 ##    6.b.  Determine the optimal number of clusters. Justify your answer. 
 ##          It may take longer running time since it uses a large dataset.
+fviz_nbclust(insurance_numeric, kmeans, method = "gap_stat") +
+  ggtitle("Optimal Number of Clusters - Gap Statistic Method") +
+  theme_test()
 
 
 ##    6.c.  Perform k-means clustering using the 3 clusters.
+kmeans_model <- kmeans(insurance_numeric, centers = 3, nstart = 25)
 
 
 ##    6.d.  Visualize the clusters in different colors.
-
+fviz_cluster(kmeans_model, data = insurance_numeric, geom = "point")
 
 
 
@@ -231,34 +334,47 @@ library(neuralnet)
 ################################################################################
 ##    7.a.  Remove the sex, smoker, and region, since they are not numerical 
 ##          values.
+insurance_nn <- insurance[, c("age", "bmi", "children", "charges")]
 
 
 ##    7.b.  Standardize the inputs using the scale() function.
+insurance_scaled <- scale(insurance_nn)
 
 
 ##    7.c.  Convert the standardized inputs to a data frame using the 
 ##          as.data.frame() function.
+insurance_scaled <- as.data.frame(insurance_scaled)
 
 
 ##    7.d.  Split the dataset into a training set containing 80% of the 
 ##          original data and the test set containing the remaining 20%.
-
+set.seed(1)
+index_nn <- sample(1:nrow(insurance_scaled), nrow(insurance_scaled)*0.8)
+train_nn <- insurance_scaled[index_nn, ]
+test_nn <- insurance_scaled[-index_nn, ]
 
 ##    7.e.  The response is charges and the predictors are age, bmi, 
 ##          and children. Please use 1 hidden layer with 1 neuron.
+nn_model <- neuralnet(charges ~ age + bmi + children, 
+                      data = train_nn, hidden = c(1))
 
 
 ##    7.f.  Plot the neural networks.
+plot(nn_model)
 
 
 ##    7.g.  Forecast the charges in the test dataset.
+pred_nn <- predict(nn_model, newdata = test_nn)
 
 
 ##    7.h.  Get the observed charges of the test dataset.
+obs_nn <- test_nn$charges
 
 
 ##    7.i.  Compute test error (MSE).
+mse_test_nn <- mean((obs_nn - pred_nn)^2)
 
+mse_test_nn
 
 
 
@@ -278,6 +394,19 @@ library(neuralnet)
 ##          test MSEs as Test.MSE and round the data in this column to 4 decimal
 ##          places. Present the formatted data to your supervisor and recommend
 ##          which model is best and why.
+model_compare <- data.frame(
+  Model.Type = c("Multiple Linear Regression", "Regression Tree", 
+                 "Random Forest", "Support Vector Machine", "Neural Network"),
+  Test.MSE = round(c(mse_test_lm, mse_test_tree, mse_test_rf, 
+                     mse_test_svm, mse_test_nn), 4)
+)
+print(model_compare)
+
+# Recommendation based on lowest MSE
+best_model <- model_compare[which.min(model_compare$Test.MSE), ]
+
+print(paste("Recommended model is", best_model$Model.Type, 
+            "with Test MSE =", best_model$Test.MSE))
 
 
 ##    8.b.  Another supervisor from the sales department has requested your help
@@ -303,6 +432,7 @@ library(neuralnet)
 ##          Follow these steps:
 ##          -------------------
 ##          i.    Copy your pruned tree model to a new variable.
+tree_copy <- pruned_tree
 
 
 ##          ii.   In your new variable, find the data.frame named "frame" and 
@@ -311,9 +441,11 @@ library(neuralnet)
 ##                tree model is named copy_of_my_pruned_tree, then the 
 ##                data frame is accessed as copy_of_my_pruned_tree$frame, 
 ##                and it works just like a normal data frame.).
+tree_copy$frame$yval <- exp(tree_copy$frame$yval)
 
 
 ##          iii.  After you reverse the log transform on the yval column, 
 ##                then replot the tree with labels.
-
+plot(tree_copy)
+text(tree_copy, pretty = 0)
 
